603cab47fb5f408d897247bab2ca2b4724760b3f
Emiliano Suñé <emiliano.sune@gmail.com>
2021-02-18 17:01:11 -0800

    Updated business bank verifier profile
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/visual-verifier/visual-verifier-deploy.overrides.sh
+++ openshift/templates/visual-verifier/visual-verifier-deploy.overrides.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 _includeFile=$(type -p overrides.inc)
+# Import ocFunctions.inc for getSecret
+_ocFunctions=$(type -p ocFunctions.inc)
 if [ ! -z ${_includeFile} ]; then
+  . ${_ocFunctions}
   . ${_includeFile}
 else
   _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
@@ -44,6 +47,8 @@ if createOperation; then
   readParameter "OIDC_RP_CLIENT_SECRET - Please provide your OIDC Identty Provider client secret. This value must match the client secret in your Identity Provider.  If left blank, a 32 character long base64 encoded value will be randomly generated using openssl:" OIDC_RP_CLIENT_SECRET $(generateKey 32) "false"
   readParameter "VC_AUTHN_PRES_REQ_CONF_ID - Please provide the presentation request configuration id to be used when authenticating.  The value may not be blank:" VC_AUTHN_PRES_REQ_CONF_ID "" "false"
   readParameter "OIDC_CLAIMS_REQUIRED - Please provide the list of claims to be checked by the verifier as a comma-separated list of values.    The value may not be blank:" OIDC_CLAIMS_REQUIRED "" "false"
+
+  parseHostnameParameter "OIDC_RP_PROVIDER_ENDPOINT" "OIDC_RP_PROVIDER_HOST"
 else
   # Secrets are removed from the configurations during update operations ...
   printStatusMsg "Update operation detected ...\nSkipping the prompts for OIDC_RP_PROVIDER_ENDPOINT, OIDC_RP_CLIENT_ID, OIDC_RP_CLIENT_SECRET, VC_AUTHN_PRES_REQ_CONF_ID, and OIDC_CLAIMS_REQUIRED secrets ... \n"
@@ -52,6 +57,9 @@ else
   writeParameter "OIDC_RP_CLIENT_SECRET" "prompt_skipped" "false"
   writeParameter "VC_AUTHN_PRES_REQ_CONF_ID" "prompt_skipped" "false"
   writeParameter "OIDC_CLAIMS_REQUIRED" "prompt_skipped" "false"
+
+  printStatusMsg "Getting OIDC_RP_PROVIDER_HOST for the ExternalNetwork definition from secret ...\n"
+  writeParameter "OIDC_RP_PROVIDER_HOST" $(getSecret "${NAME}${SUFFIX}" "oidc-rp-provider-host") "false"
 fi
 
 
